693e533ac480c08b491f818c2a7591e3f2b4bf24
Abhishek Kumar <kumara07@mscdirect.com>
2020-08-11 21:19:31 +0530

    press button

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+    var question = [
+      'What\'s your favorite color? ',
+      'What\'s your favorite animal? ',
+    ];
+   
+   void buttonPressed() {
+     print("button clicked");
+   }
+
+    @override
     Widget build(BuildContext context) {
-        return MaterialApp(home: Text('Hello Abhishek!'));
+        return MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title : Text('My first App')
+              ),
+              body: Column(
+                children: [
+                 Text('The Question'),
+                 RaisedButton(child: Text('Answer 1'), onPressed: buttonPressed),
+                 RaisedButton(child: Text('Answer 2'), onPressed: buttonPressed),
+                 RaisedButton(child: Text('Answer 3'), onPressed: buttonPressed), 
+               ],
+              ),
+            ),
+        );
     }
 }
