3d3e80993de16a9439d38276228f199a6b98a269
Abhishek Kumar <kumara07@mscdirect.com>
2020-08-14 09:24:17 +0530

    converting stateless wigdet to state ful wigdet

--- lib/main.dart
+++ lib/main.dart
@@ -5,33 +5,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// ignore: must_be_immutable
+class MyApp extends StatefulWidget {
+   @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
 
-    var question = [
-      'What\'s your favorite color? ',
-      'What\'s your favorite animal? ',
-    ];
-   
-   void buttonPressed() {
-     print("button clicked");
-   }
+class MyAppState extends State<MyApp> {
+ var question = [
+    'What\'s your favorite color? ',
+    'What\'s your favorite animal? ',
+  ];
 
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-              title : Text('My first App')
-              ),
-              body: Column(
-                children: [
-                 Text('The Question'),
-                 RaisedButton(child: Text('Answer 1'), onPressed: buttonPressed),
-                 RaisedButton(child: Text('Answer 2'), onPressed: buttonPressed),
-                 RaisedButton(child: Text('Answer 3'), onPressed: buttonPressed), 
-               ],
-              ),
-            ),
-        );
-    }
+  var questionIndex = 0;
+
+  void buttonPressed() {
+    setState(() {
+      if(questionIndex < question.length - 1) {
+        questionIndex += 1;
+      }
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My first App')),
+        body: Column(
+          children: [
+            Text(question[questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: buttonPressed),
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('answer 2 cliked')),
+            RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('answer 3 pressed');
+                }),
+          ],
+        ),
+      ),
+    );
+  }
 }
